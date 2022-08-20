2310b9dbeeb0e281c03b9e06beb9e94493dcf3e7
Avantika Sengar <avantika.sengar1996@gmail.com>
2022-08-20 21:06:14 +0530

    -

--- search_algos.py
+++ search_algos.py
@@ -1,24 +1,28 @@
 class SearchAlgo:
-    def verify(self, index):
+    @staticmethod
+    def verify(index):
         if index is None:
             print(f'Target not found')
         else:
             print(f'Target found at index {index}')
 
-    def linear_search(self, array, target):
+    @staticmethod
+    def linear_search(array, target):
         """ Return the first index of target element in the array using sequential search"""
         for i in range(0, len(array)):
             if array[i] == target:
                 return i
 
-    def linear_search_all_elements(self, array, target):
+    @staticmethod
+    def linear_search_all_elements(array, target):
         """ Return the all indexes of target element in the array """
 
         for i in range(0, len(array)):
             if array[i] == target:
                 yield i
 
-    def binary_search(self, array, target):
+    @staticmethod
+    def binary_search(array, target):
         """ Return the first index of target element in the array using binary search """
 
         array = sorted(array)
@@ -34,7 +38,8 @@ class SearchAlgo:
             else:
                 last = mid-1
 
-    def binary_search_recursive(self, array, start, last, target):
+    @staticmethod
+    def binary_search_recursive(array, start, last, target):
         """ Return the first index of target element in the array using binary search """
 
         if len(array) == 0:
@@ -45,27 +50,23 @@ class SearchAlgo:
             return mid
 
         elif array[mid] < target:
-            return self.binary_search_recursive(array, mid+1, last, target)
+            return SearchAlgo.binary_search_recursive(array, mid+1, last, target)
 
         else:
-            return self.binary_search_recursive(array, start, mid-1, target)
+            return SearchAlgo.binary_search_recursive(array, start, mid-1, target)
 
 
-# ------------------ RUN EXAMPLES --------------
+# # ------------------ RUN EXAMPLES --------------
 #
-# searchAlgo = SearchAlgo()
+# res = SearchAlgo.linear_search([3, 4, 5, 6, 1, 3, 7], 3)
+# SearchAlgo.verify(res)
 #
-# res = searchAlgo.linear_search([3, 4, 5, 6, 1, 3, 7], 3)
-# searchAlgo.verify(res)
+# res = list(SearchAlgo.linear_search_all_elements([3, 4, 5, 6, 1, 3, 7], 3))
+# SearchAlgo.verify(res)
 #
-# res = list(searchAlgo.linear_search_all_elements([3, 4, 5, 6, 1, 3, 7], 3))
-# searchAlgo.verify(res)
-#
-# res = searchAlgo.binary_search([1, 3, 3, 4, 5, 6, 7], 6)
-# searchAlgo.verify(res)
+# res = SearchAlgo.binary_search([1, 3, 3, 4, 5, 6, 7], 6)
+# SearchAlgo.verify(res)
 #
 # arr = [1, 3, 3, 4, 5, 6, 7]
-# res = searchAlgo.binary_search_recursive(arr, 0, len(arr)-1, 5)
-# searchAlgo.verify(res)
-
-
+# res = SearchAlgo.binary_search_recursive(arr, 0, len(arr)-1, 5)
+# SearchAlgo.verify(res)
